271b0b3560c3f85e7b0bc846315e1a3016317344
b-wallace <s3359666@student.rmit.edu.au>
2021-02-05 11:33:51 +1100

    first

---
+++ ms-console-tutorials/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ms_console_tutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
